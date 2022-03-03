4727c62dff823de4f5d95faed09152e6b4a9e601
jemmyyosua <yosua872@gmail.com>
2022-03-03 14:37:00 +0700

    Login Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './css/style.css';
 import icon from './assets/icon.png';
 import vector from './assets/vector.png';
 import {Row, Col, Button } from 'react-bootstrap';
+import Modals from './components/login.js'
 
 function App(){
     return  (
@@ -13,13 +13,13 @@ function App(){
            <Col className="ms-5" lg="7">
            <div className="col ms-5">
                     <img className="icon mt-4" src={icon} alt="" />
-                    <p>Sign-up now aneed subscribe to enjoy all the cool 
+                    <p>Sign-up now and subscribe to enjoy all the cool 
                        <br/> and latest books - The best book rental service 
                        <br /> provider in Indonesia</p>
                     <Col lg="8">
                     <Row className="mt-5">
-                        <Col><Button className="btn-sign px-5" variant="danger">Sign Up</Button></Col>
-                        <Col><Button className="btn-sign me-5 px-5" variant="secondary">Sign In</Button></Col>
+                        <Col></Col>
+                        <Col><Modals/></Col>
                     </Row>   
                     </Col> 
             </div>
