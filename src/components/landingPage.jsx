4bed7b6120fc0df45e3a5171eab5b09c88cc63a2
Jemmy Yosua Alie <98152020+jemmyyosua@users.noreply.github.com>
2022-04-18 14:03:24 +0700

    Update mobile view landing page

--- src/components/landingPage.jsx
+++ src/components/landingPage.jsx
@@ -1,7 +1,7 @@
 import {React, useState, useContext} from 'react'
 import icon from '../assets/icon.png'
 import vector from '../assets/vector.png'
-import {Row, Col, Button } from 'react-bootstrap'
+import {Row, Col, Button, Container } from 'react-bootstrap'
 import SignIn from './modalSignIn'
 import SignUp from './modalSignUp'
 
@@ -23,31 +23,26 @@ function LandingPage(){
 
     return (
     <>
-       <img className="img1" src={vector} alt="" width="100%" height="100%" />
-        <Col className="ms-4" lg="7">
-        <div className=" ms-5">
-                <img className="icon mt-4" src={icon} alt="" />
-                <p>Sign-up now and subscribe to enjoy all the cool 
+       <img className="img1" src={vector} alt="" style={{width : "100%", height : "100%" }} />
+        <div className="containerLanding" style={{minWidth : "100%", minHeight : "100vh"}}>
+        <Container>
+                <img className="ms-3 mt-4" style={{minWidth : "20%", maxWidth : "90%"}} src={icon} alt="" />
+                    <p className="ms-3" style={{marginTop : "-5%",width : "fit-content"}}>Sign-up now and subscribe to enjoy all the cool 
                     <br/> and latest books - The best book rental service 
                     <br /> provider in Indonesia</p>
-                <Col lg="8">
-                    <Row className="mt-5">
-                        <Col>
-                        <Button className="px-5 btn-sign" variant="danger" onClick={handleSignUpShow}>
+                    <br />
+                        <Button style={{fontOpticalSizing : "auto"}} className="px-5 ms-3 me-5 btn-sign" variant="danger" onClick={handleSignUpShow}>
                             Sign Up
                         </Button>
                         <SignUp show={signUp} handleClose={handleSignUpClose}/>
-                        </Col>
-                        <Col>
-                        <Button className="px-5 btn-sign" variant="secondary" onClick={handleSignInShow}>
+    
+                        <Button className="px-5 ms-3 btn-sign" variant="secondary" onClick={handleSignInShow}>
                             Sign In
                         </Button>
-                        <SignIn show={signIn} handleClose={handleSignInClose}/>
-                        </Col>
-                    </Row>   
-                </Col> 
+                        <SignIn show={signIn} handleClose={handleSignInClose}/> 
+        </Container>
         </div>
-        </Col>
+        
    </>
     )
     
