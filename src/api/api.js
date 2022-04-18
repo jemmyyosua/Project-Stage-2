99ba148e6fbdcbc052ec9c55fd7ce994ce5ce256
Jemmy Yosua Alie <98152020+jemmyyosua@users.noreply.github.com>
2022-04-18 17:31:08 +0700

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 export const API = () => {
     const baseUrl = process.env.REACT_APP_SERVER_URL ||
-    'https://wow-app01.herokuapp.com/wow-app' ||
+    'https://wow-app-02.herokuapp.com/wow-app' ||
     'https://localhost:5000/wow-app';
   
     const executeAPI = async (endpoint, config) => {
